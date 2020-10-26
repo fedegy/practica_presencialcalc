f291520af4e841ecf3e6b9e420280b0170b9be2a
fedegy <fede88662@gmail.com>
2020-10-25 19:55:54 -0600

    Create operaciones.py

---
+++ operaciones.py
@@ -0,0 +1,14 @@
+class operaciones:
+
+	def __init__(self,valor1,valor2):
+		self.valor1=valor1
+		self.valor2=valor2
+
+	def suma(valor1,valor2):
+		return self.valor1+self.valor2
+
+	def resta(valor1,valor2):
+		return self.valor1-self.valor2
+
+
+
